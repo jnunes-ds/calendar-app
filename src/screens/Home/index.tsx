dd6dd3c5b51e331ca2ec3b6abf13b6d3c174c0eb
Júnior Nunes <denison.junior@clicksoft.com.br>
2021-11-03 21:26:41 -0300

    creating a header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 
 import {
     Container,
+    Header,
+    Avatar,
+    Logo,
+    Chat,
+    Body,
     Title
 } from './styles'
 
@@ -10,7 +15,22 @@ export function Home(){
 
     return (
         <Container>
+            <Header>
+              <Avatar.Container>
+                <Avatar.Avatar />
+                <Avatar.Icon />
+              </Avatar.Container>
+              <Logo.Container>
+                <Logo.Logo />
+              </Logo.Container>
+              <Chat.Container>
+                <Chat.Icon />
+                <Chat.IconNotification />
+              </Chat.Container>
+            </Header>
+            <Body>
             <Title> Hello World! </Title>
+            </Body>
         </Container>
     );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,6 +1,65 @@
-import styled from 'styled-components/native';
+import styled from 'styled-components/native'
 import { RFValue } from 'react-native-responsive-fontsize';
 
-export const Container = styled.View``;
+interface AvatarProps {
+  Container: React.FC;
+  Avatar: React.FC;
+  Icon: React.FC;
+}
+
+interface LogoProps {
+  Container: React.FC;
+  Logo: React.FC;
+}
+
+interface ChatProps {
+  Container: React.FC;
+  Icon: React.FC;
+  IconNotification: React.FC;
+}
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+`;
 
 export const Title = styled.Text``;
+
+export const Header = styled.View`
+  background-color: grey;
+  width: 100%;
+  flex: 2;
+  flex-direction: row;
+`;
+
+// Avatar
+export const Avatar: AvatarProps = {} as AvatarProps;
+
+Avatar.Container = styled.View``;
+
+Avatar.Avatar = styled.Image``;
+
+Avatar.Icon = styled.Image``;
+
+//Logo
+export const Logo: LogoProps = {} as LogoProps;
+
+Logo.Container = styled.View``;
+
+Logo.Logo = styled.Image``;
+
+// Chat
+export const Chat: ChatProps = {} as ChatProps;
+
+Chat.Container = styled.View``;
+
+Chat.Icon = styled.Image``;
+
+Chat.IconNotification = styled.View``;
+
+export const Body = styled.View`
+  flex: 10;
+  width: 100%;
+`;
